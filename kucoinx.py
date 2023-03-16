710b674c5015925580a8e58a52754e2d1afc73d7
Filipe Pina <fopina@gmail.com>
2023-03-16 23:48:21 +0000

    kucoin: include subaccounts

--- kucoinx.py
+++ kucoinx.py
@@ -8,11 +8,12 @@ class CLI(BasicCLI):
         parser.add_argument('key')
         parser.add_argument('secret')
         parser.add_argument('passphrase')
+        parser.add_argument('--exclude-zeros', action='store_true', help='Do not report accounts/tokens with 0 balance')
     
     def handle(self, args):
         prices = MarketData().get_fiat_price()
         client = User(args.key, args.secret, args.passphrase)
-        wallets = client.get_account_list()
+        accounts = client.get_account_list()
 
         hass_data = {
             'state': 0,
@@ -22,16 +23,35 @@ class CLI(BasicCLI):
         }
         accum = {}
 
-        for wallet in wallets:
-            accum[wallet['currency']] = float(accum.get(wallet['currency'], 0)) + float(wallet['balance'])
+        for account in accounts:
+            if account['balance'] == '0' and args.exclude_zeros:
+                continue
+            accum[account['currency']] = float(accum.get(account['currency'], 0)) + float(account['balance'])
+
+        subs = client._request('GET', '/api/v1/sub-accounts')
+        accum_sub = {}
+        for sub in subs:
+            for k in ('mainAccounts', 'tradeAccounts', 'marginAccounts'):
+                for account in sub[k]:
+                    kn = account['currency']
+                    kv = float(account['balance'])
+                    accum[kn] = float(accum.get(kn, 0)) + kv
+                    price = float(prices.get(kn, 0))
+                    accum_sub[sub['subName']] = accum_sub.get(sub['subName'], 0) + price * kv
         
         for k, v in accum.items():
             price = float(prices.get(k, 0))
-            usd = price*float(v)
+            k = k.lower()
+            usd = price * float(v)
             print(k, v, usd)
             hass_data['state'] += usd
-            hass_data['attributes'][k.lower()] = v
-            hass_data['attributes'][f'{k.lower()}_val'] = usd
+            hass_data['attributes'][k] = v
+            hass_data['attributes'][f'{k}_val'] = usd
+
+        for k, v in accum_sub.items():
+            k = f'sub_{k}'
+            print(k, v)
+            hass_data['attributes'][k] = v
 
         print(f"\nTotal: {hass_data['state']}")
         return hass_data
