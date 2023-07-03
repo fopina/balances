94739cba99ec655377629b52bef203eef6996e31
Filipe Pina <fopina@gmail.com>
2023-07-04 00:06:43 +0100

    luna20: proper key names, stake rewards and original vesting

--- luna20.py
+++ luna20.py
@@ -3,6 +3,8 @@ import requests
 from common.cli import BasicCLI
 from common.cli.fx import CryptoFXMixin
 
+DECIMALS = 1000000
+
 
 class Client(requests.Session):
     URL = 'https://phoenix-lcd.terra.dev/cosmos/'
@@ -29,6 +31,16 @@ class Client(requests.Session):
         r.raise_for_status()
         return r.json()
 
+    def rewards(self, wallet):
+        r = self.get(f'distribution/v1beta1/delegators/{wallet}/rewards')
+        r.raise_for_status()
+        return r.json()
+
+    def vesting(self, wallet):
+        r = self.get(f'auth/v1beta1/accounts/{wallet}')
+        r.raise_for_status()
+        return r.json()
+
 
 class CLI(CryptoFXMixin, BasicCLI):
     def extend_parser(self, parser):
@@ -40,20 +52,26 @@ class CLI(CryptoFXMixin, BasicCLI):
             'attributes': {
                 'usd': 0,
                 'rate': 0,
-                'vested': 0,
+                'balance': 0,
+                'delegated': 0,
+                'rewards': 0,
                 'vesting': 0,
                 'unit_of_measurement': 'LUNA',
             },
         }
 
         c = Client()
-
         for d in c.delegations(args.wallet).get('delegation_responses', []):
-            hass_data['attributes']['vesting'] += float(d['balance']['amount']) / 1000000
+            hass_data['attributes']['delegated'] += float(d['balance']['amount']) / DECIMALS
         for d in c.balances(args.wallet).get('balances', []):
-            hass_data['attributes']['vested'] += float(d['amount']) / 1000000
+            hass_data['attributes']['balance'] += float(d['amount']) / DECIMALS
+        for d in c.rewards(args.wallet).get('rewards', []):
+            for d1 in d.get('reward', []):
+                hass_data['attributes']['rewards'] += float(d1['amount']) / DECIMALS
+        for d in c.vesting(args.wallet).get('account', {}).get('base_vesting_account', {}).get('original_vesting', []):
+            hass_data['attributes']['vesting'] += float(d['amount']) / DECIMALS
 
-        hass_data['state'] = hass_data['attributes']['vested'] + hass_data['attributes']['vesting']
+        hass_data['state'] = hass_data['attributes']['balance'] + hass_data['attributes']['delegated']
         hass_data['attributes']['rate'] = self.get_crypto_fx_rate('terra-luna-2', coinmarketcap_slugs='terra-luna-v2')
         hass_data['attributes']['usd'] = hass_data['state'] * hass_data['attributes']['rate']
         self.pprint(hass_data)
