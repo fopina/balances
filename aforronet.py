306c9333b29004aa2b7de101ba6f611c9abb1f36
Filipe Pina <fopina@gmail.com>
2023-10-21 17:18:09 +0100

    x

--- aforronet.py
+++ aforronet.py
@@ -4,6 +4,9 @@ import re
 from common.cli import BasicCLI
 
 
+TAG_RE = re.compile(r'<.+?>')
+
+
 class ClientError(Exception):
     """errors raised by client validations"""
 
@@ -16,6 +19,7 @@ class Client(requests.Session):
         self.headers.update(
             {
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0',
+                'Referer': self.URL,
             }
         )
 
@@ -32,26 +36,27 @@ class Client(requests.Session):
         data = {
             '__EVENTTARGET': '',
             '__EVENTARGUMENT': '',
-            'txtIdentificacao':	username,
-            'txtSenha':	password,
-            'btLoginAfr.x':	"0",
-            'btLoginAfr.y':	"0",
+            'txtIdentificacao': username,
+            'txtSenha': password,
+            'btLoginAfr.x': "0",
+            'btLoginAfr.y': "0",
         }
 
         m = re.findall(r'Indique o <strong>(\d+).*</strong> e <strong>(\d+).*</strong>', r.text)
         if not m:
             raise ClientError('NIF positions not found')
-        print('nif', m)
         pos = list(map(lambda x: nif[int(x) - 1], m[0]))
         data['dlPrim_Pos'] = pos[0]
         data['dlSeg_Pos'] = pos[1]
-        
+
         m = re.findall(r'<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="(.*?)" />', r.text)
         if not m:
             raise ClientError('__VIEWSTATE not found')
         data['__VIEWSTATE'] = m[0]
 
-        m = re.findall(r'<input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="(.*?)" />', r.text)
+        m = re.findall(
+            r'<input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="(.*?)" />', r.text
+        )
         if not m:
             raise ClientError('__VIEWSTATEGENERATOR not found')
         data['__VIEWSTATEGENERATOR'] = m[0]
@@ -63,99 +68,70 @@ class Client(requests.Session):
 
         r = self.post('Iimf.AforroNet.UI/services/login/Login.aspx', data=data)
         r.raise_for_status()
-        print(r.text)
+        if r.url.endswith('/Iimf.AforroNet.UI/services/login/Login.aspx'):
+            msg = re.findall(
+                r'''<TD class='msgCustomValSumm' id='MessageCustomValSumm' .*?><div id="CValSummary">(.*?)</div></TD>''',
+                r.text,
+                re.DOTALL,
+            )
+            if msg:
+                msg = msg[0]
+                msg = TAG_RE.sub(' ', msg)
+                msg = re.sub(r'\s\s+', '\n', msg)
+                msg = '; '.join(msg.strip().splitlines())
+            raise ClientError(msg)
 
-        print(data)
-        print(pos)
+    def daily_statement(self):
+        r = self.get('Iimf.AforroNet.UI/services/Consulta/Saldo.aspx')
+        r.raise_for_status()
+        subs = []
+        panels = re.findall(r'<div id="(panel\d+)">(.*?)</div>', r.text, re.DOTALL)
+        for panel in panels:
+            m = re.findall(r'<td.+?>(.*?)</td>', panel[1], re.DOTALL)
+            series = TAG_RE.sub('', m[0])
+            for i in range(1, len(m), 5):
+                sub = m[i : i + 5]
+                if len(sub) < 5:
+                    # subtotal
+                    assert 'SubTotal:' in sub[0]
+                    continue
 
-        return pos
-
-
-    def fiat_overview(self):
-        return self.get('api/fiat/account/show').json()
-
-    def supercharger_overview(self):
-        return self.get('api/supercharger/account/show').json()
-
-    def earn_overview(self):
-        return self.get('api/v2/crypto_earn/account/show').json()
+                sub.append(series)
+                for j in range(2, 5):
+                    sub[j] = float(sub[j].replace(',', '.').replace('\xa0', ''))
+                subs.append(sub)
+        return subs
 
 
 class CLI(BasicCLI):
     def extend_parser(self, parser):
-        parser.add_argument('track_id')
-        parser.add_argument('token')
+        parser.add_argument('username')
+        parser.add_argument('password')
+        parser.add_argument('nif')
 
     def handle(self, args):
         client = Client()
-        print(client.login(...))
-        return
-        mclient = MissionClient(args.track_id, args.token)
-        d = mclient.center()
-        vault_diamonds = int(d['data']['diamond_balance']['vault_amount'][0]['amount'])
-        redeem_balance = int(d['data']['redeem_balances']['total_amount']['amount'])
-        if redeem_balance > 0:
-            print('redeem diamonds')
-            dd = mclient.redeem()
-            if not dd['ok']:
-                raise Exception(d)
-
-        for track in d['data']['tracks']:
-            if track['track_name'] != 'Daily':
-                continue
-            if not track['track_active']:
-                continue
-            print('activate track')
-            d = mclient.track_activate(track['track_id'])
-            if not d['ok']:
-                raise Exception(d)
-            for m in track['missions']:
-                if m['mission_name_translation_key'] == 'mission_detail__title_check_in':
-                    if m['streak_complete']:
-                        continue
-                    print('mission: checking in')
-                    d = mclient.mission_checkin(m['mission_id'])
-                    if not d['ok']:
-                        raise Exception(d)
+        if args.insecure:
+            client.verify = False
+        client.login(args.username, args.password, args.nif)
+        subs = client.daily_statement()
 
         hass_data = {
             'state': 0,
             'attributes': {
                 'unit_of_measurement': 'EUR',
-                'diamonds': vault_diamonds,
             },
         }
-        print(f"diamonds: {vault_diamonds}")
 
-        try:
-            tokens = client.allocations()
-
-            for k in tokens['coins']:
-                _id = k['id'].lower()
-                amt = float(k['amount']['amount'])
-                val = float(k['price_native']['amount'])
-                print(f"{_id}: {val} ({amt})")
-                hass_data['attributes'][f'{_id}_amt'] = amt
-                hass_data['attributes'][f'{_id}_val'] = val
-                hass_data['state'] += val
-        except requests.exceptions.HTTPError:
-            # FIXME: ignoring this for now but need replacement endpoint!
-            print('FAILED TO GET ALLOCATIONS #FIXME')
-
-        eur = float(client.fiat_overview()['account']['balance']['amount'])
-        hass_data['attributes']['fiat_val'] = eur
-        hass_data['state'] += eur
-        print(f'fiat: {eur}')
-
-        eur = float(client.supercharger_overview()['supercharger_account']['total_balance']['amount'])
-        hass_data['attributes']['supercharger_val'] = eur
-        print(f'supercharger: {eur}')
-
-        eur = float(client.earn_overview()['crypto_earn_account']['total_balance']['amount'])
-        hass_data['attributes']['earn_val'] = eur
-        print(f'earn: {eur}')
-
-        print(f"\nTotal: {hass_data['state']}")
+        print('Subscriptions:')
+        for sub in subs:
+            _n = sub[1]
+            hass_data['attributes'][f'{_n}_series'] = sub[5]
+            hass_data['attributes'][f'{_n}_date'] = sub[0]
+            hass_data['attributes'][f'{_n}_unitprice'] = sub[2]
+            hass_data['attributes'][f'{_n}_units'] = sub[3]
+            hass_data['attributes'][f'{_n}_value'] = sub[4]
+            print(f"* {sub[1]}: {sub[0]} {sub[2:]}")
         return hass_data
 
 
