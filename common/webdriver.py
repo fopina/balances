12e678587e86f8772d515ed41ac57268e77cac2e
Filipe Pina <hzlu1ot0@duck.com>
2024-07-01 23:34:46 +0100

    x

--- common/webdriver.py
+++ common/webdriver.py
@@ -16,7 +16,7 @@ class ChromiumHelperMixin:
         options.add_argument("--disable-blink-features=AutomationControlled")
     
     def go_headless(self, options: webdriver.ChromeOptions):
-        options.headless = True
+        options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
