787e7e1fd0b223b84e2162d4d00c2878c84f6935
Filipe Pina <636320+fopina@users.noreply.github.com>
2024-09-30 00:53:04 +0100

    new image builder (#14)

---
+++ build.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S python3 -u
+
+from builder import cli
+from builder.image import Image, AlpineMixin, GCCMixin, ChromiumMixin, ChromiumLiteMixin, BaseMixin
+
+
+class MetaMask(AlpineMixin, Image): pass
+class Anchor(AlpineMixin, Image): pass
+class Celsius(AlpineMixin, Image): pass
+class Degiro(AlpineMixin, Image): pass
+class KucoinX(AlpineMixin, Image): pass
+class Financas(AlpineMixin, Image): pass
+class CaixaBreak(AlpineMixin, Image): pass
+class Luna20(AlpineMixin, Image): pass
+class CryptoCom(AlpineMixin, Image): pass
+class AforroNet(AlpineMixin, Image): pass
+
+class SnailTrail(GCCMixin, Image): pass
+
+class Plutus(ChromiumMixin, Image): pass
+class PlutusLite(ChromiumLiteMixin, Image): pass
+class IBFetch(ChromiumMixin, Image): pass
+class IBFetchLite(ChromiumLiteMixin, Image): pass
+
+class BaseAlpine(BaseMixin, Image): pass
+
+
+class BaseGCC(BaseMixin, Image):
+    FLAVOR = 'gcc'
+
+
+class BaseChromium(BaseMixin, Image):
+    FLAVOR = 'chromium'
+
+
+if __name__ == '__main__':
+    cli.cli()
---
+++ builder/image.py
@@ -0,0 +1,160 @@
+from functools import cached_property
+import subprocess
+import os
+import shlex
+from typing import Dict, List
+
+class ImageMixin:
+    """Mixin base class just to tag "families" of images"""
+
+
+class Image:
+    PLATFORMS = 'linux/amd64,linux/arm64'
+    IMAGE_BASE = None
+
+    # these are to be defined in subclasses
+    CONTEXT = None
+    DOCKERFILE = None
+    IMAGE = None
+    TAG = None
+    BUILD_ARGS = {}
+
+    def __init__(self, push=False, docker_extra=None):
+        self._push = push
+        self._docker_extra = docker_extra
+
+    def get_full_tags(self) -> List[str]:
+        """This can be subclassed to add multiple targets or name them differently"""
+        assert self.get_tag() is not None
+        assert self.get_image() is not None
+        return [f'{self.IMAGE_BASE}/{self.get_image()}:{self.get_tag()}']
+
+    def get_tag(self) -> str:
+        """This can be overriden with more complex logic"""
+        return self.TAG
+    
+    def get_image(self) -> str:
+        """This can be overriden with more complex logic"""
+        return self.IMAGE
+
+    def get_build_args(self) -> Dict[str, str]:
+        """This can be overriden with more complex logic"""
+        return self.BUILD_ARGS
+
+    def get_revision(self) -> str:
+        return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode().strip()
+
+    def get_parameter(self, name) -> str:
+        """
+        a way for image builders to get external variables
+        for now, only reads from ENV
+        """
+        return os.getenv(name)
+
+    def build_command(self) -> List[str]:
+        assert self.DOCKERFILE is not None
+        assert self.CONTEXT is not None
+
+        args = [
+            'docker', 'buildx', 'build',
+            '--build-arg', f'VCS_REF={self.get_revision()}',
+            '-f', self.DOCKERFILE,
+            self.CONTEXT,
+        ]
+
+        if self._push:
+            args.extend(['--platform', self.PLATFORMS, '--push'])
+        else:
+            args.append('--load')
+
+        for k in self.get_full_tags():
+            args.extend(['-t', k])
+
+        for k, v in self.get_build_args().items():
+            args.extend(['--build-arg', f'{k}={v}'])
+        
+        if self._docker_extra:
+            args.extend(shlex.split(self._docker_extra))
+
+        return args
+
+    def build(self) -> int:
+        return subprocess.check_call(self.build_command())
+
+
+class AlpineMixin(ImageMixin):
+    IMAGE_BASE = 'ghcr.io/fopina'
+    IMAGE = 'balances'
+    DOCKERFILE = 'docker/Dockerfile'
+    CONTEXT = '.'
+    PYTHON_VERSION = 3.9
+    FLAVOR = 'alpine'
+
+    @property
+    def service(self):
+        return self.__class__.__name__.lower()
+    
+    def get_tag(self):
+        return self.service
+    
+    def get_full_tags(self):
+        x = super().get_full_tags()
+        x.append(f'{x[0]}-{self.get_revision()}')
+        return x
+    
+    def get_revision(self) -> str:
+        return str(len(subprocess.check_output(['git', 'log', '--oneline', f'{self.service}.py', 'docker']).decode().splitlines()))
+    
+    def get_build_args(self):
+        return {
+            'TARGETBASE': f'ghcr.io/fopina/balances:base-{self.PYTHON_VERSION}-{self.FLAVOR}',
+            'ENTRY': self.service,
+        }
+
+
+class GCCMixin(AlpineMixin, ImageMixin):
+    FLAVOR = 'gcc'
+
+
+class ChromiumMixin(AlpineMixin, ImageMixin):
+    FLAVOR = 'chromium'
+
+
+class ChromiumLiteMixin(AlpineMixin, ImageMixin):
+    @property
+    def service(self):
+        assert super().service.endswith('lite')
+        return super().service[:-4]
+
+    def get_full_tags(self):
+        x = super().get_full_tags()[0]
+        return [
+            f'{x}-lite',
+            f'{x}-lite-{self.get_revision()}'
+        ]
+
+
+class BaseMixin(AlpineMixin, ImageMixin):
+    IMAGE_BASE = 'ghcr.io/fopina'
+    IMAGE = 'balances'
+    DOCKERFILE = 'docker/Dockerfile.base'
+    CONTEXT = '.'
+    PYTHON_VERSION = 3.9
+    FLAVOR = 'alpine'
+
+    def get_tag(self):
+        return f'base-{self.PYTHON_VERSION}-{self.FLAVOR}'
+
+    def get_revision(self) -> str:
+        return str(len(subprocess.check_output(['git', 'log', '--oneline', 'docker']).decode().splitlines()))
+    
+    def get_build_args(self):
+        return {
+            'BASE': f'python:{self.PYTHON_VERSION}-alpine',
+			'BASESLIM': f'python:{self.PYTHON_VERSION}-slim',
+        }
+    
+    def build_command(self):
+        cmd = super().build_command()
+        cmd.extend(['--target', self.FLAVOR])
+        return cmd
