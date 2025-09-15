c6faf3cc6d22108fb4255fcce0697711e3ee6017
Filipe Pina <hzlu1ot0@duck.com>
2025-09-15 14:10:16 +0100

    cleaner test_run

--- test_run.py
+++ test_run.py
@@ -7,49 +7,59 @@ from pathlib import Path
 
 PARAM_MAP = json.loads((Path(__file__).parent / 'test_run.json').read_text())
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    'script',
-    help='Script file to execute',
-)
-parser.add_argument('--docker', action='store_true', help='Use docker image')
-parser.add_argument("flags", nargs=argparse.REMAINDER, help="Extra arguments to forward to script")
 
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'script',
+        help='Script file to execute',
+    )
+    parser.add_argument('--docker', action='store_true', help='Use docker image')
+    parser.add_argument("flags", nargs=argparse.REMAINDER, help="Extra arguments to forward to script")
 
-if args.script.endswith('.py'):
-    args.script = args.script[:-3]
+    args = parser.parse_args()
 
-assert args.script in PARAM_MAP, 'not configured'
+    if args.script.endswith('.py'):
+        args.script = args.script[:-3]
 
-secret_key = PARAM_MAP[args.script][0]
+    assert args.script in PARAM_MAP, 'not configured'
 
-if secret_key:
-    secrets = json.loads(subprocess.check_output(['rbw', 'get', '--raw', secret_key]))
+    secret_key = PARAM_MAP[args.script][0]
 
-s_args = []
-for s_arg in PARAM_MAP[args.script][1]:
-    if s_arg[:2] == 's:':
-        s_arg = s_arg[2:]
-        if s_arg in ('username', 'password'):
-            s_args.append(secrets['data'][s_arg])
-        else:
-            for f in secrets['fields']:
-                if f['name'] == s_arg:
-                    s_args.append(f['value'])
-                    break
+    if secret_key:
+        secrets = json.loads(subprocess.check_output(['rbw', 'get', '--raw', secret_key]))
+
+    s_args = []
+    for s_arg in PARAM_MAP[args.script][1]:
+        if s_arg[:2] == 's:':
+            s_arg = s_arg[2:]
+            if s_arg in ('username', 'password'):
+                s_args.append(secrets['data'][s_arg])
             else:
-                assert False, f'{s_arg} field not found'
-    else:
-        s_args.append(s_arg)
+                for f in secrets['fields']:
+                    if f['name'] == s_arg:
+                        s_args.append(f['value'])
+                        break
+                else:
+                    assert False, f'{s_arg} field not found'
+        else:
+            s_args.append(s_arg)
 
-if args.flags:
-    s_args.extend(args.flags)
+    if args.flags:
+        s_args.extend(args.flags)
 
-try:
-    if args.docker:
-        subprocess.check_call(['docker', 'run', f'ghcr.io/fopina/balances:{args.script}'] + s_args)
-    else:
-        subprocess.check_call(['python', f'{args.script}.py'] + s_args)
-except subprocess.CalledProcessError as e:
-    exit(e.returncode)
+    try:
+        if args.docker:
+            subprocess.check_call(['docker', 'run', gen_image(args.script)] + s_args)
+        else:
+            subprocess.check_call(['python', f'{args.script}.py'] + s_args)
+    except subprocess.CalledProcessError as e:
+        exit(e.returncode)
+
+
+def gen_image(script):
+    return f'ghcr.io/fopina/balances:{script}'
+
+
+if __name__ == '__main__':
+    main()
