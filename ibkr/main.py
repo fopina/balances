2d4678d8858db930e84fa6e53e49baf1b040b3f4
Filipe Pina <hzlu1ot0@duck.com>
2026-05-10 00:48:10 +0100

    x

--- ibkr/main.py
+++ ibkr/main.py
@@ -1,6 +1,15 @@
+"""
+Scraper for IBKR.
+
+If using a secondary (read-only) user, make sure it has "Market data" access (under Trading permissions) and "TWS" under Trading platforms.
+As Secure Login System is mandatory for anyone with TWS access, you can either:
+* Add a "Mobile Authenticator App" as 2FA and provide the TOTP seed to this script to automate it; or
+* Use IB Key and get a notification every time the script runs; or
+* Convince someone from tech support to allow your account to not have SLS ^^
+"""
+
 import json
 import logging
-import re
 import time
 from dataclasses import dataclass
 from pathlib import Path
@@ -9,75 +18,36 @@ import classyclick
 import click
 import requests
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By as By
+from selenium.webdriver.support.ui import Select
 
-from common.cli_ng import 
-from common.cli_ng import SMSAuthMixin
+from balances_otp import OTPMixin
+from balances_selenium import SeleniumCLI
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 logger = logging.getLogger(__name__)
 
 
 class Client(requests.Session):
-    URL = 'https://netbenefitsww.fidelity.com/'
+    URL = 'https://www.interactivebrokers.ie/portal.proxy/v1/portal/'
 
     def __init__(self, cookies):
         super().__init__()
         self.cookies.update(cookies)
         self.headers.update(
             {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0',
-            }
-        )
-        self._client_id = None
-
-    def request(self, method: str, url: str, *args, **kwargs):
-        if not url.startswith('https://'):
-            url = f'{self.URL}{url.lstrip("/")}'
-        return super().request(method, url, *args, **kwargs)
-
-    def load_plan_data(self):
-        r = self.get('/mybenefitsww/spsnav/navigation/PlanSummary')
-        if '<title>Log In to Fidelity NetBenefits</title>' in r.text:
-            # force a proper status code!
-            r.status_code = 403
-        r.raise_for_status()
-        client_re = re.compile(r'var contextClientId =  "(\d+)";')
-        for line in r.text.splitlines():
-            m = client_re.findall(line)
-            if m:
-                self._client_id = m[0]
-                break
-        else:
-            raise click.ClickException('portfolio data not found')
-
-    def get_portofolio_accounts(self):
-        r = self.get(
-            f'/mybenefitsww/spsplanservices/stockplans/relationships/clients/{self._client_id}/portfolio-accounts'
-        )
-        r.raise_for_status()
-        return r.json()
-
-    def get_plan(self, plan_type: str, plan_ids: list[str], account_number: str, participant_type: str):
-        r = self.post(
-            f'/mybenefitsww/spsplanservices/stockplans/relationships/clients/{self._client_id}/plans',
-            json={
-                'planTypeIds': [{'planType': plan_type, 'planIds': plan_ids}],
-                'exercisablePlanTypeIds': [],
-                'userDetails': {
-                    'brokerageAccount': {'accountNumber': account_number, 'authorizedAccount': False},
-                    'filActivated': False,
-                    'participantType': participant_type,
-                },
+                'User-Agent': SeleniumCLI.DEFAULT_USER_AGENT,
             },
         )
-        r.raise_for_status()
-        return r.json()
 
-    def get_account_balance(self):
-        r = self.get('/mybenefitsww/spsaccounts/api/account-balance')
+    def request(self, method: str, url: str, *args, **kwargs):
+        url = url if url.startswith('https://') else f'{self.URL}{url}'
+        r = super().request(method, url, *args, **kwargs)
         r.raise_for_status()
-        return r.json()
+        return r
+
+    def validate(self):
+        return self.get('sso/validate').json()
 
 
 @dataclass
@@ -85,150 +55,159 @@ class Args:
     # FIXME: this should be directly in CLI but classyclick does not allow ordering arguments... split for now to control inheritance order...
     username: str = classyclick.Argument()
     password: str = classyclick.Argument()
-    screenshot: bool = classyclick.Option(help='Take screenshot on exception')
     token_file: Path = classyclick.Option(
         '-f',
-        default='.fidelity.local',
+        default='.ibkr.local',
         help='File to store current cookies',
     )
-    plan_id: str = classyclick.Option(help='If more than 1 plan, use this to specify the plan ID to track')
+    screenshot: bool = classyclick.Option(help='Take screenshot on exception')
+    account: int = classyclick.Option(help='Account ID (eg: Uxxxxxx) to monitor - required if you have more than one')
 
 
 @classyclick.command()
-class CLI(SMSAuthMixin, SeleniumCLI, Args):
-    def handle(self):
-        client = self.get_client()
-        balance = client.get_account_balance()
-        summary = client.get_portofolio_accounts()
-
-        account_number = balance['accountNumber']
-        cash_balance = float(balance['assetCurrencyBalance']['coreBalance']['value'])
-        share_balance = float(balance['assetCurrencyBalance']['marketValue']['value'])
-
-        if not self.plan_id and len(summary['planSummary']['plans']) > 1:
-            raise click.ClickException(
-                f'Multiple plans found, use --plan-id to specify which to track: {[plan["planId"] for plan in summary["planSummary"]["plans"]]}'
-            )
-
-        plan_data = None
-        for plan in summary['planSummary']['plans']:
-            assert plan['planType'] == 'RSU', f'Plan type not supported: {plan}'
-            if self.plan_id and self.plan_id != plan['planId']:
-                continue
-            plan_data = client.get_plan(plan['planType'], [plan['planId']], account_number, summary['participantType'])
-            break
-        else:
-            raise click.ClickException('No plan found, what are you tracking??')
-
-        assert len(plan_data['plans']) == 1, f'{plan["planId"]} has no data?'
-        plan_data = plan_data['plans'][0]
-        hass_data = {
-            'state': float(summary['planSummary']['totalPlanBalance']['amount']),
-            'attributes': {
-                'unit_of_measurement': 'USD',
-                'cash': cash_balance,
-                'share_balance': share_balance,
-                'shares_vested': int(float(plan_data['totalDistributedAwards'])),
-                'shares_unvested': int(float(plan_data['totalUnvestedAwards'])),
-                'lastStockPrice': float(summary['stockQuote']['price']),
-                'nextDistributionValue': float(plan_data['nextPaymentValue']),
-                'nextDistributionUnits': int(float(plan_data['nextPaymentQuantity'])),
-                'nextDistributionDate': plan_data['nextPaymentDate'],
-            },
-        }
-        self.pprint(hass_data)
-        return hass_data
-
+class CLI(OTPMixin, SeleniumCLI, Args):
     def get_client(self):
         if self.token_file.exists():
             cookies = json.loads(self.token_file.read_text())
             client = Client(cookies)
             try:
-                client.load_plan_data()
+                client.validate()
                 return client
             except requests.exceptions.HTTPError as e:
                 logger.warning('session expired: %s', e)
-        _try = 0
+
+        tries = 0
         while True:
             try:
                 cookies = self.login_and_token()
-                print(f'LOGGED IN ON TRY {_try}')
+                print(f'IBFETCH: LOGIN SUCCESS ON TRY {tries}')
                 break
             except WebDriverException as e:
-                # Message: unknown error: session deleted because of page crash
-                if 'page crash' in str(e):
-                    if _try < 3:
-                        continue
-                raise
+                tries += 1
+                if tries == 3:
+                    raise
+                print(f'IBFETCH: try {tries}: {e}')
+                time.sleep(5)
 
         with self.token_file.open('w') as f:
             f.write(json.dumps(cookies))
 
         client = Client(cookies)
-        client.load_plan_data()
         return client
 
     def login_and_token(self):
         logger.info('logging in')
         driver = self.get_webdriver()
-        driver.implicitly_wait(20)
+        driver.implicitly_wait(200)
         cookies = {}
-
         try:
-            driver.get('https://nb.fidelity.com/public/nb/worldwide/home')
-            logger.info('wait for form...')
+            driver.get('https://www.interactivebrokers.ie/sso/Login')
+            el = driver.find_element(By.NAME, 'username')
+            # accept privacy cookies
+            driver.add_cookie({'name': 'IB_PRIV_PREFS', 'value': '0%7C0%7C0'})
+            driver.get('https://www.interactivebrokers.ie/sso/Login')
+            el = driver.find_element(By.NAME, 'username')
+            time.sleep(1)
             logger.info('found login form')
-            el = driver.find_element(By.ID, 'dom-username-input')
             el.send_keys(self.username)
-
-            el = driver.find_element(By.ID, 'dom-pswd-input')
+            el = driver.find_element(By.NAME, 'password')
             el.send_keys(self.password)
-            el.send_keys('\n')
+            el.submit()
 
-            for _ in range(300):
-                print('.', end='')
-                if 'navigation/PlanSummary' in driver.current_url:
-                    break
-                if 'pvd-alert__message' in driver.page_source:
-                    el = driver.find_element(By.CSS_SELECTOR, 'p.pvd-alert__message')
-                    raise click.ClickException(f'Failed login: {el.text.strip()}')
-                if """Sorry, we can't complete this action right now.""" in driver.page_source:
-                    # rate limiting on failed logins...?
-                    raise click.ClickException('Failed login: CANNOT DO THIS RIGHT NOW???')
-                if """we'll send a temporary code to your phone""" in driver.page_source:
-                    self.fail_if_no_sms_auth()
-                    el_sms = driver.find_element(By.ID, 'dom-channel-list-primary-button')
-                    assert el_sms.text == 'Text me the code', 'Text me button does not have the right label'
+            # FIXME: missing locator for when there is only 1 2FA option (such as IB Key)
+            locator_2fa = 'div[aria-label="Select Second Factor Device"] select.xyz-multipleselect'
+            locator_done = "a[href='#/dashboard/positions']"
+            locator_error = 'div.xyz-errormessage:not(:empty)'
 
-                    # check cookie wall first
-                    el_cookie = driver.find_element(By.ID, 'onetrust-accept-btn-handler')
-                    el_cookie.click()
+            el = driver.find_element(By.CSS_SELECTOR, ','.join([locator_2fa, locator_done, locator_error]))
 
-                    el_sms.click()
+            # condition order matters
+            if el.tag_name == 'select':
+                otp_code = self.get_otp_code()
+                if otp_code is not None:
+                    Select(el).select_by_visible_text('Mobile Authenticator App')
+                    el = driver.find_element(By.CSS_SELECTOR, 'input[aria-label="Mobile Authenticator App Code"]')
+                    el.send_keys(self.get_otp_code())
+                    el.submit()
+                else:
+                    # otherwise select IBKey and wait for it to be approved
+                    Select(el).select_by_visible_text('IB Key')
 
-                    sms_code = self.prompt_code()
-                    # do not ask again
-                    el_otp = driver.find_element(By.ID, 'dom-trust-device-checkbox')
-                    if not el_otp.is_selected():
-                        # why is not clickable by the browser...?
-                        driver.execute_script('arguments[0].click();', el_otp)
+                # check again for the other 2
+                el = driver.find_element(By.CSS_SELECTOR, ','.join([locator_done, locator_error]))
 
-                    el_otp = driver.find_element(By.ID, 'dom-otp-code-input')
-                    el_otp.send_keys(sms_code)
-                    el_otp.send_keys('\n')
-                time.sleep(0.1)
-            else:
-                raise Exception('timed out logging in...')
-
+            if el.tag_name == 'div':
+                raise click.ClickException(el.get_attribute('innerHTML'))
+            time.sleep(1)
+            logger.info('logged in!')
             cookies.update({c['name']: c['value'] for c in driver.get_cookies()})
+        except click.ClickException:
+            raise
         except Exception:
             if self.screenshot:
-                driver.save_screenshot(self.screenshot)
+                driver.save_screenshot('ibfetch-debug.png')
+            print(f'== SOURCE ==\n{driver.page_source}\n== SOURCE END ==')
             raise
         finally:
             driver.quit()
-
         return cookies
+
+    def handle(self):
+        client = self.get_client()
+        accounts = client.get('portfolio/accounts').json()
+
+        if not accounts:
+            raise click.ClickException('No accounts?')
+        if len(accounts) == 1:
+            account = accounts[0]
+        else:
+            ids = ', '.join([account['id'] for account in accounts])
+            if not self.account:
+                raise click.ClickException(
+                    f'You have more than 1 account, you need to use --account and choose one of: {ids}'
+                )
+            for account in accounts:
+                if account['id'] == self.account:
+                    break
+            else:
+                raise click.ClickException(f'Account {self.account} not found, choose onf of {ids}')
+
+        acc_id = account['accountId']
+        # overriden by NetLiquidation currency but keeping as a note
+        currency = account['currency']
+        net_val = 0
+
+        summary = client.get(f'portfolio2/{acc_id}/summary').json()
+        for entry in summary:
+            if entry['key'] == 'NetLiquidation':
+                net_val = entry['monetaryValue']
+                currency = entry['currency']
+                break
+        else:
+            raise click.ClickException('NetLiquidation not found')
+
+        hass_data = {
+            'state': net_val,
+            'attributes': {
+                'unit_of_measurement': currency,
+            },
+        }
+
+        positions = client.get(f'portfolio2/{acc_id}/positions').json()
+        for pos in positions:
+            ticker = pos['description'].lower()
+            val = pos['marketValue']
+            hass_data['attributes'][f'{ticker}_size'] = pos['position']
+            hass_data['attributes'][f'{ticker}_val'] = val
+            hass_data['attributes'][f'{ticker}_ccy'] = pos['currency']
+
+        ledger = client.get(f'portfolio/{acc_id}/ledger').json()
+        for cur, curd in ledger.items():
+            ticker = cur.lower()
+            hass_data['attributes'][f'{ticker}_fiat'] = curd['cashbalance']
+
+        self.pprint(hass_data)
+        return hass_data
 
 
 if __name__ == '__main__':
