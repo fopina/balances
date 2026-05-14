78cc9e29661388fca4f6362e42df6da28e73667f
Filipe Pina <hzlu1ot0@duck.com>
2026-05-14 09:42:13 +0100

    Run balance collectors as modules

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-# temporary until all are migrated to modules
-if [ -e /${BALANCE_ENTRY}/main.py ]; then
-    exec python -u /${BALANCE_ENTRY}/main.py "$@"
-else
-    exec python -u /${BALANCE_ENTRY} "$@"
-fi
+exec python -u /${BALANCE_ENTRY} "$@"
